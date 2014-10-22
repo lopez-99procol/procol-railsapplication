8540daab5d95d6e6f6ef295809512338af8072b5
lopez <t.krueger@mac.com>
2014-10-22 18:28:56 +0200

    Better cowsay Demo

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,5 @@ ActiveRecord::Schema.define(version: 20141022160739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
